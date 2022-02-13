d9b141bb82292c344668cd243e27fb4c5f077674
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-02-12 22:07:29 -0500

    commit tree 1a88652a5b215cc41f5e2dc89a8207f0c03f6334

---
+++ index.js
@@ -0,0 +1,22 @@
+let Scooter = class {
+    constructor(year, color, model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+}
+
+let Driver = class {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+}
+
+let PickupLocation = class {
+    constructor(address, city) {
+        this.address = address;
+        this.city = city;
+    }
+}
